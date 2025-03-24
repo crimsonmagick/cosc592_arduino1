0e3de42a8e208a3d3eab4015f9899ef8710b9b46
Welby Seely <welbyseely@gmail.com>
2025-03-24 01:57:01 -0400

    Complex - works

--- hexdump.sh
+++ hexdump.sh
@@ -1,13 +1,5 @@
-
 # Set the root directory for model files
-#MODEL_DIR="models"
-#MODEL_DIR="models_complex"
 MODEL_DIR="models_complex"
-#MODEL_DIR="models_ib"
-
-# Install xxd if it is not available
-# apt-get -qq install xxd
-
 # Save the file as a C source file
 xxd -i "$MODEL_DIR/sine_model_quantized.tflite" > "$MODEL_DIR/sine_model_quantized.cc"
 
